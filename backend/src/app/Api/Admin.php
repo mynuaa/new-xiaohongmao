4e88c14b02f2e4217409ab9888dbcc2215a03396
Seiry <yuwenjie66@126.com>
2018-09-19 17:44:37 +0800

    登录代码迁移

--- backend/src/app/Api/Admin.php
+++ backend/src/app/Api/Admin.php
@@ -15,23 +15,16 @@ use App\Domain\Activity as DActivity;
 use App\Domain\Join as DJoin;
 
 /**
- * 前端接口
+ * 管理员功能
  *
  * @author: Seiry Yu
  */
 
-class Front extends Api {
+class Admin extends Api {
 
 
 	public function getRules() {
         return [
-            'index' => [
-                'username' => [
-                    'name' => 'username', 
-                    'default' => 'PhalApi', 
-                    'desc' => '用户名'
-                ],
-            ],
             'beforeLogin' => [
                 'stuid' => [
                     'name' => 'stuid', 
@@ -78,28 +71,6 @@ class Front extends Api {
                     'type' => 'string',
                 ]
             ],
-            'allActivity' => [
-                'from' => [
-                    'name' => 'from', 
-                    'desc' => '分页起始',
-                    'type' => 'int',
-                    'default' => 0, 
-                ],
-                'pagenum' => [
-                    'name' => 'pagenum', 
-                    'desc' => '页面大小',
-                    'type' => 'int',
-                    'default' => 20, 
-                ]
-            ],
-            'getActivity' => [
-                'id' => [
-                    'name' => 'id', 
-                    'desc' => '活动id',
-                    'type' => 'int',
-                    'require' => true,
-                ]
-            ],
         ];
 	}
     
@@ -111,59 +82,53 @@ class Front extends Api {
         $this->Act = new DActivity();
         $this->Join = new DJoin();
     }
-
-	/**
-	 * 默认接口服务
-     * @desc 默认接口服务，当未指定接口服务时执行此接口服务
-	 * @return string title 标题
-	 * @return string content 内容
-	 * @return string version 版本，格式：X.X.X
-	 * @return int time 当前时间戳
-     * @exception 400 非法请求，参数传递错误
-	 */
-	public function index() {
-        return $this->Front->index();
-        return [
-            'title' => 'Hello ' . $this->username,
-            'version' => PHALAPI_VERSION,
-            'time' => $_SERVER['REQUEST_TIME'],
-        ];
+    
+    /**
+     * 登录之前获取chanllenge
+     *
+     * @return strings 挑战id
+     */
+    public function beforeLogin(){
+        return $this->GTCode->startCaptchaServlet($this->stuid);
     }
     
     /**
-     * 获取所有志愿活动
+     * 登录
      *
      * @return void
      */
-    public function allActivity(){
-        $re = $this->Act->gets($this->from, $this->pagenum);
 
-        return $re;
+    public function login(){
+        
+        /*$geetest = $this->GTCode->verifyLoginServlet($this->challenge, $this->validate, $this->seccode, $this->stuid);
+        if($geetest !== true){
+            throw new Exception('验证码错误', 500);
+        }*/
+        
+        $ded = $this->Ded->verify($this->stuid, $this->passwd);
+        if($ded === false){
+            throw new Exception('密码错误', 403);
+        }
+
+        $admin = $this->User->isAdmin($this->stuid);
+        return $this->User->encode($ded['name'], $this->stuid, $admin);
+
+        if($this->Ded->binded($this->stuid)){//已经绑定 老用户
+            //返回jwt
+            $admin = $this->User->isAdmin($this->stuid);
+            return $this->User->encode($ded['name'], $this->stuid, $admin);
+        }else{
+            // ？是否要激活？
+            throw new Exception('请确认绑定', 200);
+        }
+
+        //判断是否是新注册
+        //正常的业务逻辑
     }
 
-    /**
-     * 按id获取活动
-     *
-     * @return void
-     */
-    public function getActivity(){
-        $re = $this->Act->get($this->id);
 
-        return $re;
-    }
-
-    /**
-     * 获取用于展示的数据
-     *
-     * @return array
-     */
-    public function showData(){
-        $re = [];
-        $re['allTimeLong'] = $this->Join->countAll();
-        $re['mouthLong'] = $this->Join->countMonth();
-        $re['allUser'] = $this->User->countAll();
-        $re['averageTimeLong'] = $this->Join->average();
-
-        return $re;
+    private function isAdmin(){
+        $re = $this->User->decode($this->jwt);
+        
     }
 }
