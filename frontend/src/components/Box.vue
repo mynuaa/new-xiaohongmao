6d07a1df0b315a6e5db39c23746df7533ead621a
SUER0_0 <563511736@qq.com>
2018-09-19 20:40:24 +0800

    再修改一下盒子样式

---
+++ frontend/src/components/Box.vue
@@ -0,0 +1,100 @@
+<template>
+    <div id="box">
+        <div id="header">
+            <div>公告</div>
+            <a href='/'>more</a>
+        </div>
+        <div id="body">
+          <ul>
+            <li v-for="item in items">
+              <div class="label">{{item.label}}</div>
+              <div class="origin" v-bind:class="{'two':istrue}">{{item.origin}}</div>
+              <div class="date">{{item.date}}</div>
+            </li>
+          </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "Box",
+  props: {
+      // isTwo:string
+  },
+  data: function() {
+    return {
+      istrue:true,  
+      items: [
+        {
+          label: "南京航空航天大学志愿",
+          origin: "校青协",
+          date: "2014-02-03"
+        },
+        {
+          label: "南京航空航天大学志愿",
+          origin: "",
+          date: "2014-02-08"
+        },
+        {
+          label: "南京航空航天大学志愿",
+          origin: "",
+          date: "2014-02-08"
+        },
+        {
+          label: "南京航空航天大学志愿",
+          origin: "",
+          date: "2014-02-08"
+        },
+        {
+          label: "南京航空航天大学志愿",
+          origin: "",
+          date: "2014-02-08"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style <style lang="scss" scoped>
+#header{
+  height: 20%;
+  line-height: 100%;
+  div{
+    display: inline-block;
+  }
+  a{
+    float: right;
+  }
+}
+#body{
+  
+  height: 80%;
+  ul{
+    margin: 0px;
+    padding: 0px;
+    li{
+      text-align: left;
+      list-style-type: none;
+      text-decoration: none;
+      .label{
+        display: inline-block;
+        width: 50%;
+      }
+      .origin{
+        display: inline-block;
+        width: 30%;
+      }
+      .date{
+        display: inline-block;
+        width: 20%;
+      }
+    }
+  }
+}
+.two{
+  display: none;
+}
+</style>
+
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,17 +1,45 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <div class="body-left">
+      <Box  class="box-announcement"></Box>
+      <Box  class="box-comment"></Box>
+    </div>
+    <div class="body-right">
+      <Box  class="box-activity"></Box>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Box from '../components/Box.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    Box
   }
 }
 </script>
+
+<style scoped lang="scss">
+.home {
+  display: flex;
+  flex-direction: row;
+  // text-align: center;
+  justify-content: center;
+}
+.body-left {
+  width: 40%;
+  display: flex;
+  flex-direction: column;
+}
+.body-right {
+  width: 40%;
+  display: flex;
+  flex-direction: column;
+}
+.box-announcement {
+  height: 50%;
+}
+</style>
