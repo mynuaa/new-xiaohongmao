3b2f99926f228cc3a2a7851754dcde7f1d2fae95
qinchi <qinchi@nuaa.edu.cn>
2018-09-20 12:05:30 +0800

    个人信息页面，关闭eslint，卸载husky

--- management/src/views/information/index.vue
+++ management/src/views/information/index.vue
@@ -1,17 +1,17 @@
 <template>
-	<div class="app">
-    	<el-table :data="tableData" style="width: 100%">
-      		<el-table-column prop="uname" label="姓名" width="180"></el-table-column>
-      		<el-table-column prop="gender" label="性别" width="180"></el-table-column>
-     		<el-table-column prop="stuid" label="学号"></el-table-column>
-    	</el-table>
-  	</div>
+  <div class="app">
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column prop="uname" label="姓名" width="180"/>
+      <el-table-column prop="gender" label="性别" width="180"/>
+      <el-table-column prop="stuid" label="学号"/>
+    </el-table>
+  </div>
 </template>
 <script>
-import axios from 'axios';
+import axios from 'axios'
 export default {
-	name: 'information',
-	data() {
+  name: 'Information',
+  data() {
     	return {
       		tableData: [{
         		uname: '666',
@@ -20,18 +20,18 @@ export default {
       		}]
     	}
 	  },
-  	created () {
+  	created() {
     	this.fetchData()
 	  },
-	methods:{
-		fetchData(){
-			axios.post('http://my.nuaa.edu.cn/xiaohongmao2/?service=App.User.GetInfo', {
-      		'jwt':'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1bmFtZSI6InNlaXJ5Iiwic3R1aWQiOiIwMzE2MzAyMjYiLCJhZG1pbiI6eyJsZXZlbCI6M319.EZrwdqZRItLmiGrq6wAbPjtZXgVlX5NnvKfYCB4gx4I'
-    		})  
-    		.then((response)=>{
-				this.tableData = response.data
-    		});
-		}
-	}
+  methods: {
+    fetchData() {
+      axios.post('http://my.nuaa.edu.cn/xiaohongmao2/?service=App.User.GetInfo', {
+      		'jwt': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1bmFtZSI6InNlaXJ5Iiwic3R1aWQiOiIwMzE2MzAyMjYiLCJhZG1pbiI6eyJsZXZlbCI6M319.EZrwdqZRItLmiGrq6wAbPjtZXgVlX5NnvKfYCB4gx4I'
+    		})
+    		.then((response) => {
+          this.tableData = response.data
+    		})
+    }
+  }
 }
 </script>
