5d252f98c5c33d13f280cbd38d578bd042a1ea26
Seiry <yuwenjie66@126.com>
2018-09-21 19:00:29 +0800

    更新jwt

--- backend/src/app/Api/Admin.php
+++ backend/src/app/Api/Admin.php
@@ -384,7 +384,7 @@ class Admin extends Api {
  * @return void
  */
     public function makejwt(){
-        return $this->User->encode('seiry', '031630226', ['level' => 1, 'yuan' => 3]);
+        //return $this->User->encode('seiry', '031630226', ['level' => 1, 'yuan' => 3]);
         return $this->User->encode('seiry', '031630226', ['level' => 3]);
     }
 }
