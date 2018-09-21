ceb7013cafdb3787295075f09a18bb4b3e200fad
Seiry <yuwenjie66@126.com>
2018-09-21 17:21:43 +0800

    + hoter add get

--- backend/src/app/Model/Hoster.php
+++ backend/src/app/Model/Hoster.php
@@ -13,5 +13,23 @@ class Hoster {
         return $re;
     }
 
-    
+    public function getAll(){
+        $re = di()->db->select('hoster', '*');
+
+        return $re;
+    }
+    public function add($name, $nickname){
+        $re = di()->db->insert('hoster', [
+            'hostname' => $name,
+            'hostnickname' => $nickname
+        ]);
+
+        if(di()->db->error()[0] == 0){
+            return di()->db->id();
+        }else{
+            return false;
+        }
+    }
+
+
 }
