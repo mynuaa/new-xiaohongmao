05f0b245531e6e5c60f27a916fc2782cdf67aa70
Seiry <yuwenjie66@126.com>
2018-09-22 10:15:01 +0800

    add medoo

---
+++ transTool/trans.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once './Medoo.php';
+
+use Medoo\Medoo;
+
+$old = new Medoo([
+    'database_type' => 'mysql',
+    'database_name' => 'volunteer',
+    'server' => 'localhost',
+    'username' => 'root',
+    'password' => 'root',
+    'charset' => 'utf8mb4',
+    'port' => 3306,
+    'prefix' => '',
+]);
+
+$new = new Medoo([
+    'database_type' => 'mysql',
+    'database_name' => 'newvolunteer',
+    'server' => 'localhost',
+    'username' => 'root',
+    'password' => 'root',
+    'charset' => 'utf8mb4',
+    'port' => 3306,
+    'prefix' => '',
+]);
