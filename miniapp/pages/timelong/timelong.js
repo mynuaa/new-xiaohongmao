baab2f1cafb22f9545688838158fcff4898acc6b
Seiry <yuwenjie66@126.com>
2018-11-17 16:25:50 +0800

    update 认证页面

--- miniapp/pages/timelong/timelong.js
+++ miniapp/pages/timelong/timelong.js
@@ -1,4 +1,11 @@
 // pages/timelong.js
+/**
+ * 写在前边
+ * 2018.11.17
+ * 
+ * 信息化处的新的验证方式导致了api的无法访问，所以.....就先写好前端
+ * 
+ */
 Page({
 
   /**
@@ -15,8 +22,9 @@ Page({
    * 生命周期函数--监听页面加载
    */
     onLoad: function (options) {
+        
         const stuid = options.stuid || '031630226'
-        wx.request({
+        /*wx.request({
             url: '//my.nuaa.edu.cn/xiaohongmao2/api/?s=Wechat.GetJoin',
             data: {
                 stuid: stuid
@@ -27,7 +35,9 @@ Page({
                     join: re.data.data.join
                 })
             }
-        })
+        })*/
+        
+        
     },
 
   /**
